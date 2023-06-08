e9d6ad98ae9a23ea82605f238aa06d0f0b2b244e
Syoyo Fujita <syoyo@lighttransport.com>
2023-06-09 03:41:06 +0900

    define nanoz_uncompress().

--- nanozdec.h
+++ nanozdec.h
@@ -3,9 +3,22 @@
 #ifndef NANOZDEC_H_
 #define NANOZDEC_H_
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef enum nanoz_status {
+  NANOZ_SUCCESS = 0,
+  NANOZ_ERROR = -1, // general error code.
+  NANOZ_ERROR_INVALID_ARGUMENT = -2,
+} nanoz_status_t;
+
+nanoz_status_t nanoz_uncompress(const unsigned char *src_addr,
+                                const uint64_t src_size,
+                                unsigned char *dst_addr,
+                                uint64_t *uncompressed_size);
 
 #ifdef __cplusplus
 }
@@ -13,7 +26,42 @@ extern "C" {
 
 #if defined(NANOZDEC_IMPLEMENTATION)
 
+#define WUFFS_IMPLEMENTATION
 
-#endif // NANOZDEC_IMPLEMENTATION
+#define WUFFS_CONFIG__STATIC_FUNCTIONS
+
+#define WUFFS_CONFIG__MODULES
+#define WUFFS_CONFIG__MODULE__BASE
+#define WUFFS_CONFIG__MODULE__CRC32
+#define WUFFS_CONFIG__MODULE__DEFLATE
+
+#include "wuffs-v0.3.c"
+
+nanoz_status_t nanoz_uncompress(const unsigned char *src_addr,
+                                const uint64_t src_size,
+                                unsigned char *dst_addr,
+                                uint64_t *uncompressed_size) {
+
+  if (!src_addr) {
+    return NANOZ_ERROR_INVALID_ARGUMENT;
+  }
+
+  if (src_size < 4) {
+    return NANOZ_ERROR_INVALID_ARGUMENT;
+  }
+
+  if (!dst_addr) {
+    return NANOZ_ERROR_INVALID_ARGUMENT;
+  }
+
+  if (!uncompressed_size) {
+    return NANOZ_ERROR_INVALID_ARGUMENT;
+  }
+
+  // TODO
+  return NANOZ_ERROR;
+}
+
+#endif  // NANOZDEC_IMPLEMENTATION
 
 #endif /* NANOZDEC_H_ */
---
+++ test_nanozdec.cc
@@ -0,0 +1,9 @@
+#define NANOZDEC_IMPLEMENTATION
+#include "nanozdec.h"
+
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+  return EXIT_SUCCESS;
+}
