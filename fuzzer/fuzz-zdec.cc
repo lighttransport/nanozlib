7581f246843265413323a96e9163812e51645285
Syoyo Fujita <syoyo@lighttransport.com>
2023-06-11 23:16:53 +0900

    W.I.P.

---
+++ fuzzer/fuzz-zdec.cc
@@ -0,0 +1,53 @@
+#include <cstdint>
+#include <cstring>
+#include <memory>
+#include <vector>
+
+#define NANOZDEC_IMPLEMENTATION
+#include "nanozdec.h"
+
+static int uncompress_fuzz_main(const uint8_t *data, size_t size) {
+  // first 8 bytes: uncompressed size
+  // second 8 bytes: compressed size
+  // reamaining: compressed data.
+
+  if (size < 16) {
+    return -1;
+  }
+
+  uint64_t uncompressedSize;
+  uint64_t compressedSize;
+
+  memcpy(&uncompressedSize, data, 8);
+  memcpy(&compressedSize, data+8, 8);
+
+  // FIXME: Currently up to 4GB
+  if ((uncompressedSize < 1) || (uncompressedSize > 1024*1024*4)) {
+    return 0;
+  }
+
+  if ((compressedSize < 4) || (compressedSize > 1024*1024*4)) {
+    return 0;
+  }
+
+  if (compressedSize > (size -16)) {
+    return 0;
+  }
+
+  std::vector<unsigned char> dst;
+  dst.resize(uncompressedSize);
+
+  nanoz_status_t s = nanoz_uncompress(reinterpret_cast<const unsigned char *>(data + 8),
+                                compressedSize, uncompressedSize, dst.data());
+
+  (void)s;
+
+  return 0;
+
+}
+
+extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const* data,
+                                      std::size_t size) {
+  return uncompress_fuzz_main(data, size);
+}
+
--- test_nanozdec.cc
+++ test_nanozdec.cc
@@ -4,11 +4,64 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <vector>
 #include <iostream>
 
 int main(int argc, char **argv) {
   if (argc < 3) {
-    std::cout << "test compressed_data.bin uncompresed_size\n";
+    std::cout << "test compressed_data.bin uncompresed_size (dump_uncompressed_data)\n";
   }
+
+  bool dump = false;
+
+  // TODO: Use string -> uint64
+  std::string filename = argv[1];
+  uint64_t uncompressed_size = atoi(argv[2]);
+
+  std::vector<char> uncompressed_data;
+  uncompressed_data.resize(uncompressed_size);
+
+  if (argc > 3) {
+    dump = true;
+  }
+
+  std::ifstream f(filenme.c_str(), std::ifstream::binary);
+  if (!f) {
+    std::cerr << "File not found or invalid.\n";
+    return EXIT_FAILURE;
+  }
+
+  f.peek();
+  if (!f) {
+    std::cerr << "Invalid file.\n";
+    return EXIT_FAILURE;
+  }
+
+  f.seekg(0, f.end);
+  size_t sz = static_cast<size_t>(f.tellg());
+  f.seekg(0, f.beg);
+
+  std::vector<unsigned char> src_buf;
+  src_buf.resize(sz);
+
+  f.read(src_buf.data(), static_cast<std::streamsize>(sz));
+
+  if (!f) {
+    std::cerr << "Failed to read file.\n";
+    return EXIT_FAILURE;
+  }
+
+  nanoz_status_t ret = nanoz_uncompress(src_buf.data(), sz, uncompresed_size, uncompressed_data.data());
+  if (ret != NANOZ_SUCCESS) {
+    std::cerr << "nanoz error: " << int(ret) << "\n";
+    return EXIT_FAILURE;
+  }
+
+  if (dump) {
+    std::string s(uncompressed_data.begin(), uncompressed_data.end());
+    std::cout << s << "\n";
+  }
+
+
   return EXIT_SUCCESS;
 }
